38a141258a970b8647cfe30756d8c8d10f5fc393
Andrew Trice <amtrice@us.ibm.com>
2022-12-13 13:45:16 -0500

    updated output refs
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -8,7 +8,7 @@ resource local_file write_outputs {
     namespace   = module.wazi_devspaces.namespace
     server_name = module.wazi_devspaces.server_name
     layer       = module.wazi_devspaces.layer
-    layer_dir   = module.wazi_devspaces.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
+    layer_dir   = module.wazi_devspaces.layer == "infrastructure" ? "1-infrastructure" : (module.wazi_devspaces.layer == "services" ? "2-services" : "3-applications")
     type        = module.wazi_devspaces.type
   })
 }
